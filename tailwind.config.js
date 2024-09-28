58b63e16f9c755d5623b37af0372512c1a1d31eb
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-09-28 22:16:44 +1000

    add margin for nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./**/*.{html,js,jsx,ts,tsx}',
+    '!./node_modules/**/*'
+  ],
   theme: {
     extend: {},
   },
